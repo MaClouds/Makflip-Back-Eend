9091ab5eb45ab0bb5f1bcef20f821f4c9c0a3d1b
macaulay famous <macaulayfamous@gmail.com>
2024-08-22 16:14:32 +0100

    changed database

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors'); // Import CORS package
 const app = express();
 
-const Db = "mongodb+srv://macaulayfamous:beejaymac@cluster0.1sqrhdg.mongodb.net/";
-// Db = "mongodb+srv://Makflip:Makflip123@cluster0.jm0pctz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// const Db = "mongodb+srv://macaulayfamous:beejaymac@cluster0.1sqrhdg.mongodb.net/";
+Db = "mongodb+srv://Makflip:Makflip123@cluster0.jm0pctz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // Db = "mongodb+srv://macaulayfamous:beejaymac@cluster0.kkep68t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Import routes
